e6626e078d74cbe7c923f7b9d4b5b416c4d81215
Marvin Botchway <marvinbotchway@outlook.com>
2023-12-07 11:56:20 +0000

    Display Book Titles In A List On The Page.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+const bookListContainer = document.querySelector(".container");
+const bookList = document.createElement("ul");
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -8,22 +11,26 @@ function Book(title, author, pages, read) {
 }
 
 myLibrary.push(new Book(
-    "Things Fall Apart",
-    "Chinua Achebe",
-    "155",
-    true));
-myLibrary.push(new Book(
-    "Purple Hibiscus",
-    "Chimamanda Ngozi Adichie",
-    "336",
-    false));
-myLibrary.push(new Book(
-    "The Thing Around Your Neck",
-    "Chimamanda Ngozi Adichie",
-    240,
-    false));
+"Things Fall Apart", "Chinua Achebe", "155", true));
 
-console.log(myLibrary);
+myLibrary.push(new Book(
+"Purple Hibiscus", "Chimamanda Ngozi Adichie", "336", false));
+
+myLibrary.push(new Book(
+"The Thing Around Your Neck", "Chimamanda Ngozi Adichie", "240", false));
+
+
+
+myLibrary.forEach((book) => {
+    let listItem = document.createElement("li");
+    let bookCard = document.createElement("div");
+    bookCard.textContent = book.title;
+   
+    listItem.appendChild(bookCard);
+    bookList.appendChild(listItem);
+});
+
+bookListContainer.appendChild(bookList);
 
 function addBookToLibrary() {
 }
