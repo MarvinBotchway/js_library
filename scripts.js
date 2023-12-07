17ea4728bbf18dfb497200c564ceab1a02c68aa9
Marvin Botchway <marvinbotchway@outlook.com>
2023-12-07 14:15:18 +0000

    Add Image or Placeholder To Cards
    Added a property to the Book constructor which takes an image url.
    If no image url is entered it uses a placeholder image.
    Added Styling.

--- scripts.js
+++ scripts.js
@@ -3,29 +3,40 @@ const bookList = document.createElement("ul");
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, imgUrl, author, pages, read) {
+    if (!imgUrl) imgUrl = "https://placehold.co/258x375";
     this.title = title;
+    this.imgUrl = imgUrl
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
 myLibrary.push(new Book(
-"Things Fall Apart", "Chinua Achebe", "155", true));
+"Things Fall Apart",
+"https://upload.wikimedia.org/wikipedia/en/6/65/ThingsFallApart.jpg",
+"Chinua Achebe", "155", true));
 
 myLibrary.push(new Book(
-"Purple Hibiscus", "Chimamanda Ngozi Adichie", "336", false));
+"Purple Hibiscus",
+"",
+"Chimamanda Ngozi Adichie", "336", false));
 
 myLibrary.push(new Book(
-"The Thing Around Your Neck", "Chimamanda Ngozi Adichie", "240", false));
+"The Thing Around Your Neck",
+"",
+"Chimamanda Ngozi Adichie", "240", false));
 
 
 
 myLibrary.forEach((book) => {
     let listItem = document.createElement("li");
     let bookCard = document.createElement("div");
-    bookCard.innerHTML = 
-    `<h3>${book.title}</h3>` +
+    bookCard.innerHTML =
+    `<div>
+        <h3>${book.title}</h3>
+        <img src=${book.imgUrl}>
+    </div>` +
     `<p><span>Author</span>: ${book.author}</p>` +
     `<p><span>Pages</span>: ${book.pages}</p>` +
     `<p><span>Read</span>: ${book.read}</p>`;
