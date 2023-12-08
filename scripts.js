e31ab7490e56021752731c579699878c1d1b9bf4
Marvin Botchway <marvinbotchway@outlook.com>
2023-12-08 12:06:31 +0000

    Hook Up New Book Form.
    Added the ability to add new books to the booklist.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
 const bookListContainer = document.querySelector(".container");
 const addBookButton = document.querySelector("#add-book");
 const dialog = document.querySelector("dialog");
+const newBookForm = document.querySelector("#new-book-form");
 
 const bookList = document.createElement("ul");
+
+newBookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+    newBookForm.reset();
+    dialog.close();
+});
 
 addBookButton.addEventListener("click", () => dialog.showModal());
 
@@ -33,27 +41,44 @@ myLibrary.push(new Book(
 "Chimamanda Ngozi Adichie", "240", false));
 
 function addBookToLibrary() {
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let imageUrl = document.querySelector("#image-url").value;
+    let read = document.querySelector("#read").checked;
 
+    myLibrary.push(new Book (title, imageUrl, author, pages, read));
+
+    createBookListHTML();
 } 
 
 
+function createBookListHTML() {
+    if(bookList.hasChildNodes) removeBookListHTML();
+    myLibrary.forEach((book) => {
+        let listItem = document.createElement("li");
+        let bookCard = document.createElement("div");
+        bookCard.innerHTML =
+        `<div>
+            <h3>${book.title}</h3>
+            <img src=${book.imgUrl}>
+        </div>` +
+        `<p><span>Author</span>: ${book.author}</p>` +
+        `<p><span>Pages</span>: ${book.pages}</p>` +
+        `<p><span>Read</span>: ${book.read}</p>`;
+        bookCard.classList += "card";
+    
+        listItem.appendChild(bookCard);
+        bookList.appendChild(listItem);
+    });
+}
 
-myLibrary.forEach((book) => {
-    let listItem = document.createElement("li");
-    let bookCard = document.createElement("div");
-    bookCard.innerHTML =
-    `<div>
-        <h3>${book.title}</h3>
-        <img src=${book.imgUrl}>
-    </div>` +
-    `<p><span>Author</span>: ${book.author}</p>` +
-    `<p><span>Pages</span>: ${book.pages}</p>` +
-    `<p><span>Read</span>: ${book.read}</p>`;
-    bookCard.classList += "card";
-   
-    listItem.appendChild(bookCard);
-    bookList.appendChild(listItem);
-});
+function removeBookListHTML() {
+    while(bookList.firstChild) {
+        bookList.removeChild(bookList.firstChild);
+    }
+}
 
+createBookListHTML();
 bookListContainer.appendChild(bookList);
 
